29bc8556071cb70e095c9a9856b9e71b6dc0a77f
Malkiel Asher <masher@syr.edu>
2020-04-17 20:39:50 -0400

    added subreddit search functionality

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import praw
 import config
 
-api_id = config.api_id[:-1]
+api_id = config.api_id
 api_secret = config.api_secret;
 
 reddit = praw.Reddit(client_id = api_id,
@@ -10,27 +10,38 @@ reddit = praw.Reddit(client_id = api_id,
 
 #TO-DO: Parse posts from time/date with certain inputs, ex what countries we parse
 
+print(api_id)
+print(api_secret)
+
+
+#================================Malkiel
+#Unix Time: https://www.epochconverter.com/
+
+
+for submission in reddit.subreddit('coronavirus').hot(limit=1000):
+    print(submission.title)
+    print("-----------------")
+    #https://api.pushshift.io/reddit/search/submission/?subreddit=coronavirus&sort_type=created_utc&size=500&after=90d&before=20d
+# listOfPosts = [reddit.subreddit("coronavirus")(limit=1)]#getPostSomehow(), TO-DO
 #================================
-listOfPosts = [reddit.subreddit("coronavirus")(limit=1)]#getPostSomehow(), TO-DO
-#================================
 
-numPosts = 0 #for daily score breakdown
-
-for post in listOfPosts:
-  peoplePosted = [] #keep track of UNIQUE posters
-
-  if post.author not in peoplePosted: #if not in list, add to day count
-    numPosts = numPosts + 1
-
-  peoplePosted.append(post.author)
-
-  postName = post.name #title of post
-  postScore = post.score #karma on the posts
-  postURL = post.url #url the post links to
-  postNumComments = post.num_comments #number of comments in the post
-  postComments = post.comments(limit=5) #provides list of comments
-  #==============================
-  postSentiment = 0 #TODO
-  #==============================
+# numPosts = 0 #for daily score breakdown
+#
+# for post in listOfPosts:
+#   peoplePosted = [] #keep track of UNIQUE posters
+#
+#   if post.author not in peoplePosted: #if not in list, add to day count
+#     numPosts = numPosts + 1
+#
+#   peoplePosted.append(post.author)
+#
+#   postName = post.name #title of post
+#   postScore = post.score #karma on the posts
+#   postURL = post.url #url the post links to
+#   postNumComments = post.num_comments #number of comments in the post
+#   postComments = post.comments(limit=5) #provides list of comments
+#   #==============================
+#   postSentiment = 0 #TODO
+#   #==============================
 
 
