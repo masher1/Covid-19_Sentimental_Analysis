0af044c8015b23b34cc4425cefef5ef0f807de50
Malkiel Asher <masher@syr.edu>
2020-04-19 18:30:46 -0400

    Cleaned up the code
    - Added comments
    - Deleted legacy code

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import praw
 import config
 import pandas as pd
 import requests
@@ -7,10 +6,11 @@ import csv
 import time
 import datetime
 from textblob import TextBlob
+
 # Global Variables
 subStats = {}
-small_data = []
 
+#function used to collect the data from reddit given a country and start and end date
 def getPushshiftData(query, after, before):
     url = 'https://api.pushshift.io/reddit/search/submission/?title='+str(query)+'&size=1000&after='+str(after)+'&before='+str(before)+'&subreddit=coronavirus'
     print(url)
@@ -18,8 +18,8 @@ def getPushshiftData(query, after, before):
     data = json.loads(r.text)
     return data['data']
 
+#Function used to collect data for each submission
 def collectSubData(subm):
-
     subData = list()  # list to store data points
     title = subm['title']
     titleBlob = TextBlob(title)
@@ -53,24 +53,18 @@ def updateSubs_file():
         for sub in subStats:
              a.writerow(subStats[sub][0])
              upload_count += 1
-
         print(str(upload_count) + " submissions have been uploaded")
 
+#Main Method
 if __name__ == '__main__':
-    api_id = "rezu8NeSSbAqQw" #EXPIRED
-    api_secret = "ogniJ3HqR5ORM_4o4H0x6p9q5OE" #EXPIRED
-
-    reddit = praw.Reddit(client_id=api_id,
-                         client_secret=api_secret,
-                         user_agent='<console:reddit_bot:0.0.1')
-    query = "Italy"  # Country Name [21 dec 1576946801,28 dec 1577551601, jan 4 1578156401,jan 11 1578761201,jan 18 1579366001,jan 25 1579970801]
+    query = "Italy"  # Country Name
     weeks = [1580515200, 1581120000, 1581724800, 1582329600, 1582934400, 1583539200, 1584144000, 1584748800, 1585353600, 1585958400, 1586563200] #Feb - Early Apr
     weekNum = 0
-    big_data = []
+    big_data = [] # will contain a list of lists of submissions 
 
     # make API call to get all post from each week and put them into bigData ==> list[list[Object{}],list[Object{}],...]
     while (weekNum < len(weeks)-1):
-        data = getPushshiftData(query, weeks[weekNum], weeks[weekNum+1])
+        data = getPushshiftData(query, weeks[weekNum], weeks[weekNum+1]) # will contain all submissions per week
         print("Number of submissions for week #" + str(weekNum+1) + ": " + str(len(data)))
         big_data.append(data)
         for submissions in data:
@@ -78,31 +72,18 @@ if __name__ == '__main__':
         weekNum += 1
 
     print("Number of weeks gathered in total: " + str(len(big_data)))
-    small_data = big_data
-    sentimentScores = []
-
+small    
     iterateNum = 0
-    sentimentScore = 0.0
-
-    while big_data:
-        sentimentQuantity = len(big_data[iterateNum])
-        sentimentScores = sentimentQuantity
-        iterateNum += 1
-        if (iterateNum >= len(big_data) - 1):
-            break
-
-    iterateNum = 0
-
+   
+    #Perform sentiment analysis on the title of each post
     while (iterateNum < len(big_data) - 1):
         for submission in big_data[iterateNum]:
             title = TextBlob(submission['title'])
             submission['sentimentScore'] = title.sentiment.polarity
-
         iterateNum += 1
-
-    sentimentScore = sentimentScore / sentimentQuantity
-    print("Score: "+str(sentimentScore))
-    count = sum([len(listElem) for listElem in small_data])
+    
+    count = sum([len(listElem) for listElem in big_data])
     print(str(count) + " submissions have been added to list")
-
+    
+    #Update the .CSV file
     updateSubs_file()
